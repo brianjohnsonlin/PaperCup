aade344a19beea3a0f47844e7e5ae9376d6cb69f
Brian Lin <bjlin@ucsc.edu>
2016-02-26 13:12:57 -0800

    revision1

--- PaperCup/MainMenu.Designer.cs
+++ PaperCup/MainMenu.Designer.cs
@@ -45,7 +45,7 @@
             // masterTable
             // 
             this.masterTable.ColumnCount = 1;
-            this.masterTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.masterTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.masterTable.Controls.Add(this.buttonsTable, 0, 0);
             this.masterTable.Controls.Add(this.bottomTable, 0, 1);
             this.masterTable.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -53,9 +53,9 @@
             this.masterTable.Margin = new System.Windows.Forms.Padding(0);
             this.masterTable.Name = "masterTable";
             this.masterTable.RowCount = 2;
-            this.masterTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90.46243F));
-            this.masterTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.537572F));
-            this.masterTable.Size = new System.Drawing.Size(1670, 947);
+            this.masterTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.masterTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.masterTable.Size = new System.Drawing.Size(614, 311);
             this.masterTable.TabIndex = 3;
             // 
             // buttonsTable
@@ -66,21 +66,22 @@
             this.buttonsTable.Controls.Add(this.createButton, 0, 0);
             this.buttonsTable.Controls.Add(this.joinButton, 1, 0);
             this.buttonsTable.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.buttonsTable.Location = new System.Drawing.Point(100, 96);
-            this.buttonsTable.Margin = new System.Windows.Forms.Padding(100, 96, 100, 96);
+            this.buttonsTable.Location = new System.Drawing.Point(0, 0);
+            this.buttonsTable.Margin = new System.Windows.Forms.Padding(0);
             this.buttonsTable.Name = "buttonsTable";
             this.buttonsTable.RowCount = 1;
             this.buttonsTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.buttonsTable.Size = new System.Drawing.Size(1470, 664);
+            this.buttonsTable.Size = new System.Drawing.Size(614, 261);
             this.buttonsTable.TabIndex = 2;
             // 
             // createButton
             // 
             this.createButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.createButton.Location = new System.Drawing.Point(60, 58);
-            this.createButton.Margin = new System.Windows.Forms.Padding(60, 58, 60, 58);
+            this.createButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.createButton.Location = new System.Drawing.Point(30, 30);
+            this.createButton.Margin = new System.Windows.Forms.Padding(30, 30, 30, 30);
             this.createButton.Name = "createButton";
-            this.createButton.Size = new System.Drawing.Size(615, 548);
+            this.createButton.Size = new System.Drawing.Size(247, 201);
             this.createButton.TabIndex = 1;
             this.createButton.Text = "Create a Room";
             this.createButton.UseVisualStyleBackColor = true;
@@ -88,10 +89,11 @@
             // joinButton
             // 
             this.joinButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.joinButton.Location = new System.Drawing.Point(795, 58);
-            this.joinButton.Margin = new System.Windows.Forms.Padding(60, 58, 60, 58);
+            this.joinButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.joinButton.Location = new System.Drawing.Point(337, 30);
+            this.joinButton.Margin = new System.Windows.Forms.Padding(30, 30, 30, 30);
             this.joinButton.Name = "joinButton";
-            this.joinButton.Size = new System.Drawing.Size(615, 548);
+            this.joinButton.Size = new System.Drawing.Size(247, 201);
             this.joinButton.TabIndex = 2;
             this.joinButton.Text = "Join a Room";
             this.joinButton.UseVisualStyleBackColor = true;
@@ -99,63 +101,62 @@
             // bottomTable
             // 
             this.bottomTable.ColumnCount = 3;
-            this.bottomTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.51495F));
-            this.bottomTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 83.48505F));
-            this.bottomTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 336F));
+            this.bottomTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.bottomTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.bottomTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
             this.bottomTable.Controls.Add(this.nicknameLabel, 0, 0);
             this.bottomTable.Controls.Add(this.settingsButton, 2, 0);
             this.bottomTable.Controls.Add(this.nicknameBox, 1, 0);
             this.bottomTable.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.bottomTable.Location = new System.Drawing.Point(20, 875);
-            this.bottomTable.Margin = new System.Windows.Forms.Padding(20, 19, 20, 19);
+            this.bottomTable.Location = new System.Drawing.Point(0, 261);
+            this.bottomTable.Margin = new System.Windows.Forms.Padding(0);
             this.bottomTable.Name = "bottomTable";
             this.bottomTable.RowCount = 1;
-            this.bottomTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.bottomTable.Size = new System.Drawing.Size(1630, 53);
+            this.bottomTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.bottomTable.Size = new System.Drawing.Size(614, 50);
             this.bottomTable.TabIndex = 3;
             this.bottomTable.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel3_Paint);
             // 
             // nicknameLabel
             // 
+            this.nicknameLabel.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.nicknameLabel.AutoSize = true;
-            this.nicknameLabel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.nicknameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.nicknameLabel.Location = new System.Drawing.Point(6, 0);
-            this.nicknameLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.nicknameLabel.Location = new System.Drawing.Point(14, 15);
             this.nicknameLabel.Name = "nicknameLabel";
-            this.nicknameLabel.Size = new System.Drawing.Size(201, 53);
+            this.nicknameLabel.Size = new System.Drawing.Size(83, 20);
             this.nicknameLabel.TabIndex = 2;
             this.nicknameLabel.Text = "Nickname:";
             this.nicknameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // settingsButton
             // 
+            this.settingsButton.BackgroundImage = global::PaperCup.Properties.Resources.icon_ios7_gear_128;
+            this.settingsButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.settingsButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.settingsButton.Location = new System.Drawing.Point(1299, 6);
-            this.settingsButton.Margin = new System.Windows.Forms.Padding(6);
+            this.settingsButton.Location = new System.Drawing.Point(567, 3);
             this.settingsButton.Name = "settingsButton";
-            this.settingsButton.Size = new System.Drawing.Size(325, 41);
+            this.settingsButton.Size = new System.Drawing.Size(44, 44);
             this.settingsButton.TabIndex = 0;
-            this.settingsButton.Text = "button3";
             this.settingsButton.UseVisualStyleBackColor = true;
             // 
             // nicknameBox
             // 
-            this.nicknameBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.nicknameBox.Location = new System.Drawing.Point(219, 6);
-            this.nicknameBox.Margin = new System.Windows.Forms.Padding(6);
+            this.nicknameBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.nicknameBox.Location = new System.Drawing.Point(103, 15);
             this.nicknameBox.Name = "nicknameBox";
-            this.nicknameBox.Size = new System.Drawing.Size(1068, 31);
+            this.nicknameBox.Size = new System.Drawing.Size(458, 20);
             this.nicknameBox.TabIndex = 1;
             // 
             // MainMenu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1670, 947);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(614, 311);
             this.Controls.Add(this.masterTable);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(6);
+            this.MinimumSize = new System.Drawing.Size(630, 350);
             this.Name = "MainMenu";
             this.Text = "PaperCup";
             this.Load += new System.EventHandler(this.Form1_Load);
