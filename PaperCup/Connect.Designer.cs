5ed3d43f7fc6bc21e8fcd8a11c764c0d91399484
Brian Lin <bjlin@ucsc.edu>
2016-03-02 16:21:17 -0800

    revision6

--- PaperCup/Connect.Designer.cs
+++ PaperCup/Connect.Designer.cs
@@ -42,13 +42,13 @@
             // 
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Controls.Add(this.connectingImage, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.Cancel, 0, 3);
             this.tableLayoutPanel1.Controls.Add(this.label1, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.progressBar1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.connectingImage, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 4;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 82.74112F));
@@ -61,10 +61,8 @@
             // connectingImage
             // 
             this.connectingImage.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.connectingImage.BackgroundImage = global::PaperCup.Properties.Resources.connecting_icon1;
             this.connectingImage.Image = global::PaperCup.Properties.Resources.connecting_icon1;
             this.connectingImage.Location = new System.Drawing.Point(56, 8);
-            this.connectingImage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.connectingImage.Name = "connectingImage";
             this.connectingImage.Size = new System.Drawing.Size(164, 69);
             this.connectingImage.TabIndex = 0;
@@ -74,7 +72,7 @@
             // 
             this.Cancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
             this.Cancel.Location = new System.Drawing.Point(88, 149);
-            this.Cancel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Cancel.Margin = new System.Windows.Forms.Padding(2);
             this.Cancel.Name = "Cancel";
             this.Cancel.Size = new System.Drawing.Size(100, 23);
             this.Cancel.TabIndex = 2;
@@ -97,7 +95,7 @@
             this.progressBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.progressBar1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.progressBar1.Location = new System.Drawing.Point(2, 89);
-            this.progressBar1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(2);
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(272, 11);
             this.progressBar1.TabIndex = 3;
@@ -109,7 +107,7 @@
             this.ClientSize = new System.Drawing.Size(276, 174);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Connect";
