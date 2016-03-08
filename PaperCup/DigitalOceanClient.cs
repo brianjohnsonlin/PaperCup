0daf825d5ad349e79f9a079cf6e95ec4c1fcfa19
Brian Lin <brianlin07@gmail.com>
2016-03-08 11:26:43 -0800

    added digitalocean api

---
+++ PaperCup/DigitalOceanClient.cs
@@ -0,0 +1,49 @@
+﻿using DigitalOcean.API.Clients;
+using DigitalOcean.API.Http;
+using RestSharp;
+
+namespace DigitalOcean.API {
+    public class DigitalOceanClient : IDigitalOceanClient {
+        public static readonly string DigitalOceanApiUrl = "https://api.digitalocean.com/v2/";
+        private readonly IConnection _connection;
+
+        public DigitalOceanClient(string token) {
+            var client = new RestClient(DigitalOceanApiUrl) {
+                UserAgent = "digitalocean-api-dotnet"
+            };
+            client.AddDefaultHeader("Authorization", string.Format("Bearer {0}", token));
+
+            _connection = new Connection(client);
+
+            Actions = new ActionsClient(_connection);
+            DomainRecords = new DomainRecordsClient(_connection);
+            Domains = new DomainsClient(_connection);
+            DropletActions = new DropletActionsClient(_connection);
+            Droplets = new DropletsClient(_connection);
+            ImageActions = new ImageActionsClient(_connection);
+            Images = new ImagesClient(_connection);
+            Keys = new KeysClient(_connection);
+            Regions = new RegionsClient(_connection);
+            Sizes = new SizesClient(_connection);
+        }
+
+        #region IDigitalOceanClient Members
+
+        public IRateLimit Rates {
+            get { return _connection.Rates; }
+        }
+
+        public IActionsClient Actions { get; private set; }
+        public IDomainRecordsClient DomainRecords { get; private set; }
+        public IDomainsClient Domains { get; private set; }
+        public IDropletActionsClient DropletActions { get; private set; }
+        public IDropletsClient Droplets { get; private set; }
+        public IImageActionsClient ImageActions { get; private set; }
+        public IImagesClient Images { get; private set; }
+        public IKeysClient Keys { get; private set; }
+        public IRegionsClient Regions { get; private set; }
+        public ISizesClient Sizes { get; private set; }
+
+        #endregion
+    }
+}
---
+++ PaperCup/Exceptions/ApiException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace DigitalOcean.API.Exceptions {
+    public class ApiException : Exception {
+        private readonly IDictionary<int, string> _errors = new Dictionary<int, string> {
+            { 401, "Access Denied" },
+            { 404, "Not Found" },
+            { 429, "Rate Limit Exceeded" }
+        };
+
+        public HttpStatusCode StatusCode { get; private set; }
+
+        public override string Message {
+            get { return _errors.ContainsKey((int)StatusCode) ? _errors[(int)StatusCode] : "Unknown API error"; }
+        }
+
+        public ApiException(HttpStatusCode statusCode) {
+            StatusCode = statusCode;
+        }
+    }
+}
